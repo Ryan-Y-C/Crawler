15d39ef27e1b9394f6f9b502438f2f64ecb6f5dd
Administrator <2433005622@qq.com>
2020-06-11 16:41:11 +0800

    重构代码
    使代码简洁明了。

--- src/main/java/com/github/hcsp/Crawler.java
+++ src/main/java/com/github/hcsp/Crawler.java
@@ -9,6 +9,7 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -28,37 +29,84 @@ public class Crawler {
         while (!linkPool.isEmpty()) {
             //当获取到链接后将链接删除，ArrayList从尾部删除更有效率
             String link = linkPool.remove(linkPool.size() - 1);
-            if (!processedLink.contains(link) || !link.contains("passport") && link.contains("sina.cn") && (link.contains("news.sina") || "https://sina.cn".equals(link))) {
-                CloseableHttpClient httpclient = HttpClients.createDefault();
-                if (link.startsWith("//")) {
-                    link = "https:" + link;
-                }
-                System.out.println(link);
-                HttpGet httpGet = new HttpGet(link);
-                httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537");
+            if (processedLink.contains(link)) {
+                continue;
+            }
+            if (isInterestingLink(link)) {
 
-                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                    System.out.println(response1.getStatusLine());
-                    HttpEntity entity1 = response1.getEntity();
-                    String html = EntityUtils.toString(entity1);
-                    Document doc = Jsoup.parse(html);
-                    ArrayList<Element> links = doc.select("a[href]");
-                    for (Element aTag : links) {
-                        linkPool.add(aTag.attr("href"));
-                    }
-                    //如果是新闻页则存储数据
-                    ArrayList<Element> articleTags = doc.select("article");
-                    if (!articleTags.isEmpty()) {
-                        for (Element articleTag : articleTags) {
-                            String title = articleTag.select("h1.art_tit_h1").text();
-                            System.out.println(title);
-                        }
-                    }
-                    processedLink.add(link);
-                }
+                Document doc = httpGetAndParseHtml(link);
+
+                doc.select("a[href]").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+
+                storeIntoDataBaseIfIsNewsPage(doc);
+
+                processedLink.add(link);
+
+//                CloseableHttpClient httpclient = HttpClients.createDefault();
+//                if (link.startsWith("//")) {
+//                    link = "https:" + link;
+//                }
+//                System.out.println(link);
+//                HttpGet httpGet = new HttpGet(link);
+//                httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537");
+
+//                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+//                    System.out.println(response1.getStatusLine());
+//                    HttpEntity entity1 = response1.getEntity();
+//                    String html = EntityUtils.toString(entity1);
+//                    Document doc = Jsoup.parse(html);
+
+//                }
             } else {
                 continue;
             }
         }
+    }
+
+    private static void storeIntoDataBaseIfIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTag.select("h1.art_tit_h1").text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        System.out.println(link);
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537");
+
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return isNotSurveyPage(link) && isNotLoginPage(link) && (isNewsPage(link) || isIndexPage(link));
+    }
+
+    private static boolean isNotSurveyPage(String link) {
+        return !link.contains("survey");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport");
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina");
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
     }
 }
