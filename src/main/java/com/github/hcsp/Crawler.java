a4e670d54fcc20007e060b719456027e67592abf
Administrator <2433005622@qq.com>
2020-06-15 23:58:10 +0800

    修复编码引起的BUG。

--- src/main/java/com/github/hcsp/Crawler.java
+++ src/main/java/com/github/hcsp/Crawler.java
@@ -1,5 +1,6 @@
 package com.github.hcsp;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,83 +12,91 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+
+
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
+
 
 public class Crawler {
+    private static final String USER = "ryan";
+
+    private static final String PASSWORD = "123";
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
     public static void main(String[] args) throws IOException, SQLException {
         String url = "jdbc:h2:file:J:\\Crawler\\news";
-        String user = "ryan";
-        String password = "123";
-        Connection connection = DriverManager.getConnection(url, user, password);
-        //从数据库加载即将待处理的链接的代码
-        List<String> linkPool = loadUrlsFromDataBase(sql("links_to_be_processed"), connection);
+        Connection connection = DriverManager.getConnection(url, USER, PASSWORD);
 
-        //从数据库加载已经处理的链接的代码
-//        Set<String> processedLink = new HashSet<>(loadUrlsFromDataBase(sql("links_already_processed"), connection));
-
-
-            while (!linkPool.isEmpty()) {
-                //处理完成后更新数据库
-                String link = URLDecoder.decode(linkPool.remove(linkPool.size() - 1), "UTF-8");
-                removeFromDataBase(connection, link);
-                boolean flag=false;
-                try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM LINKS_ALREADY_PROCESSED WHERE LINK=?")) {
-                    statement.setString(1,link);
-                    ResultSet resultSet = statement.executeQuery();
-                    while (resultSet.next()){
-                        flag=true;
-                    }
-                }
-
-                if (flag) {
-                    continue;
-                }
-                if (isInterestingLink(link)) {
-
-                    Document doc = httpGetAndParseHtml(link);
-
-                    for (Element aTag : doc.select("a[href]")) {
-                        String href = aTag.attr("href");
-                        try (PreparedStatement statement = connection.prepareStatement("insert into LINKS_TO_BE_PROCESSED (link)values ( ? )")) {
-                            statement.setString(1, href);
-                            statement.executeUpdate();
-                        }
-                    }
-
-                    storeIntoDataBaseIfIsNewsPage(doc);
-
-                    try (PreparedStatement statement = connection.prepareStatement("insert into LINKS_ALREADY_PROCESSED (link)values ( ? )")) {
-                        statement.setString(1, link);
-                        statement.executeUpdate();
-                    }
-
-//                    processedLink.add(link);
-                } else {
-                    continue;
-                }
+        while (true) {
+            List<String> linkPool = loadUrlsFromDataBase("select link from links_to_be_processed", connection);
+            if (linkPool.isEmpty()) {
+                break;
             }
+            String link = URLDecoder.decode(linkPool.remove(linkPool.size() - 1), "UTF-8");
+            removeFromDataBase(connection, link);
+            System.out.println(link);
+            if (isLinkProcessed(connection, link)) {
+                continue;
+            }
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                //将该页面的所有链接放入待处理的数据库中
+                parseUrlsFromPageAndStoreIntoDataBase(connection, doc);
+
+                //将当前新闻页面存储在news数据表中
+                storeIntoDataBaseIfIsNewsPage(doc);
+
+                //将当前链接放入已处理的数据库中
+                System.out.println(link);
+                insertLinkIntoDataBase(connection, link, "INSERT INTO LINKS_ALREADY_PROCESSED (LINK)VALUES ( ? )");
+            }
+        }
     }
 
-    private static void removeFromDataBase(Connection connection, String link) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("delete from LINKS_TO_BE_PROCESSED where link=?")) {
+    //将该页面的所有链接放入待处理的数据库中
+    private static void parseUrlsFromPageAndStoreIntoDataBase(Connection connection, Document doc) throws SQLException, UnsupportedEncodingException {
+        for (Element aTag : doc.select("a[href]")) {
+            String href = URLDecoder.decode(aTag.attr("href"), "UTF-8");
+            insertLinkIntoDataBase(connection, href, "INSERT INTO LINKS_TO_BE_PROCESSED (LINK)VALUES ( ? )");
+        }
+    }
+
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        boolean flag = false;
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM LINKS_ALREADY_PROCESSED WHERE LINK=?")) {
+            statement.setString(1, link);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                flag = true;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return flag;
+    }
+
+    private static void insertLinkIntoDataBase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, link);
             statement.executeUpdate();
         }
     }
 
-    private static String sql(String table) {
-        return "select link from " + table;
+    private static void removeFromDataBase(Connection connection, String link) throws SQLException {
+        insertLinkIntoDataBase(connection, link, "delete from LINKS_TO_BE_PROCESSED where link=?");
     }
 
     private static List<String> loadUrlsFromDataBase(String sql, Connection connection) throws SQLException {
         ArrayList<String> results = new ArrayList<>();
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = statement.executeQuery();
+        try (PreparedStatement statement = connection.prepareStatement(sql); ResultSet resultSet = statement.executeQuery()) {
             while (resultSet.next()) {
                 results.add(resultSet.getString(1));
             }
@@ -95,6 +104,7 @@ public class Crawler {
         return results;
     }
 
+    //将新闻页面存储在news数据表中
     private static void storeIntoDataBaseIfIsNewsPage(Document doc) {
         ArrayList<Element> articleTags = doc.select("article");
         if (!articleTags.isEmpty()) {
@@ -111,7 +121,6 @@ public class Crawler {
             link = "https:" + link;
         }
 
-        System.out.println(link);
         HttpGet httpGet = new HttpGet(link);
         httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537");
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
